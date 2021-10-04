0f54967b75b762c862fff11914d38c649902cefb
vsol <vsol.sil@gmail.com>
2021-10-03 21:49:47 -0300

    barra de navegación fija con javascript

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,6 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
     scrollNav();
+    fixedNav();
 });
+
+function fixedNav () {
+    const header = document.querySelector('header');
+    const seccionInfo = document.querySelector('.info');
+    const video = document.querySelector('.video');
+
+    const intersection = new IntersectionObserver(function(entries) {
+        if (entries[0].isIntersecting) {
+            header.classList.remove('fixed');
+        } else {
+            header.classList.add('fixed');
+        }
+    })
+
+    intersection.observe(video);
+}
 
 function scrollNav () {
     const enlaces = document.querySelectorAll('.navbar a');
